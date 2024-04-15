1a0ea4608737b61623e23c239fd2f91fdf67b372
HarshithaVennapusa <iamharshitha1213@gmail.com>
2024-04-15 18:48:03 +0530

    My first commit

---
+++ Product.java
@@ -0,0 +1,50 @@
+package org.jsp.userproduct.dto;
+
+import org.springframework.stereotype.Component;
+
+@Component(value="product")
+public class Product {
+	private int id;
+	private String name;
+	private String description;
+	private String brand;
+	private String category;
+	private double cost;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getBrand() {
+		return brand;
+	}
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public double getCost() {
+		return cost;
+	}
+	public void setCost(double cost) {
+		this.cost = cost;
+	}
+	
+}
